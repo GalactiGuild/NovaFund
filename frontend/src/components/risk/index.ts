d8cfe31cd4aa9575ce1e1d193c82722ab35a911a
Yusuf Falade <faladeyusuf54@gmail.com>
2026-02-25 07:34:08 +0100

    ...

--- frontend/src/components/risk/index.ts
+++
@@ -1,4 +0,0 @@
-export { RiskDashboard }    from "./RiskDashboard";
-export { RiskScoreCard }    from "./RiskScoreCard";
-export { RiskFactorChart }  from "./RiskFactorChart";
-export { RealTimeMonitor }  from "./RealTimeMonitor";
